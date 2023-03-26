07017c0a75ea85d60764cbde21f45f1a2c5b35b0
Heinz Stecher <122392148+DataKaiCR@users.noreply.github.com>
2023-03-26 12:53:45 -0600

    Publish initial version of tutorial
    This initial commit has the first draft of the tutorial for optimizing
    pandas data handling. You should expect grammar mistakes, and lots of
    features and better code employment usage.
    
    The release has a library called fasting_pandas for some simple modules
    for benchmarking or creating and manipulating the datasets.
    
    It is divided into 3 main jupyter notebooks that have all the
    information and comments for the tutorial.
    
    In future releases it is expected to have better module features for
    benchmarking, better analytics, and some docs for additional
    information.

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .fasting_pandas import datasets
+from .fasting_pandas import utils
+from .fasting_pandas import PROJECT_DIR, DATA_DIR
+
+__all__ = ['datasets', 'utils','PROJECT_DIR', 'DATA_DIR']
